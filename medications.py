c9115d1f7c30f27903d3fdf8ef59b5a3941e517e
bayer2517 <kbairamov2517@gmail.com>
2024-08-12 22:59:15 -0500

    Your commit message here

--- medications.py
+++
@@ -1,9 +0,0 @@
-meds = {"1 : Rizatriptan 5mg",
-        "2 : Rizatriptan 10mg",
-        "3: Sumatriptan 5mg",
-        "4: Sumatriptan 10mg", 
-        "albuterol",
-          "flonase"}
-
-
-
--- messages.py
+++ messages.py
@@ -3,5 +3,4 @@ start_text = "Hi I can help you with day supply!"
 help_text = """Avalible commands:
 /start - Start bot
 /help - help (this message)
-/joke - get random joke
 """
